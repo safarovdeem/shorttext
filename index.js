35abb6814ba90da862bab60fa59888076f896e60
safarovdeem <safarovdeem@yandex.com>
2021-06-28 10:14:18 +0500

    change index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function makeShort(text, words) {
+export function makeShort(text, words) {
     let arr = text.split(' ')
     if (arr.length > words) {
         text = arr.slice(0, words).join(' ')
